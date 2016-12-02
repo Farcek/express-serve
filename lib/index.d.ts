d9d4ac50a65424feac7de6a9d7e4037ca16a831d
Farcek <farcek@gmail.com>
2016-12-02 14:42:49 +0800

    add swig viewEngine
    add crossdomain allowing

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -10,7 +10,6 @@ export declare enum AccessLogType {
 }
 export interface IOptions {
     port?: number;
-    errorStack?: boolean;
 }
 export interface IErrorHandle {
     (err: errors.IError, req: express.Request, res: express.Response, next: express.NextFunction): void;
@@ -18,18 +17,85 @@ export interface IErrorHandle {
 export interface IAction {
     (): Promise<void>;
 }
+export interface IServeStatic {
+    /**
+     * Set how "dotfiles" are treated when encountered. A dotfile is a file or directory that begins with a dot (".").
+     * Note this check is done on the path itself without checking if the path actually exists on the disk.
+     * If root is specified, only the dotfiles above the root are checked (i.e. the root itself can be within a dotfile when when set to "deny").
+     * The default value is 'ignore'.
+     * 'allow' No special treatment for dotfiles
+     * 'deny' Send a 403 for any request for a dotfile
+     * 'ignore' Pretend like the dotfile does not exist and call next()
+     */
+    dotfiles?: string;
+    /**
+     * Enable or disable etag generation, defaults to true.
+     */
+    etag?: boolean;
+    /**
+     * Set file extension fallbacks. When set, if a file is not found, the given extensions will be added to the file name and search for.
+     * The first that exists will be served. Example: ['html', 'htm'].
+     * The default value is false.
+     */
+    extensions?: string[];
+    /**
+     * Let client errors fall-through as unhandled requests, otherwise forward a client error.
+     * The default value is false.
+     */
+    fallthrough?: boolean;
+    /**
+     * By default this module will send "index.html" files in response to a request on a directory.
+     * To disable this set false or to supply a new index pass a string or an array in preferred order.
+     */
+    index?: boolean | string | string[];
+    /**
+     * Enable or disable Last-Modified header, defaults to true. Uses the file system's last modified value.
+     */
+    lastModified?: boolean;
+    /**
+     * Provide a max-age in milliseconds for http caching, defaults to 0. This can also be a string accepted by the ms module.
+     */
+    maxAge?: number | string;
+    /**
+     * Redirect to trailing "/" when the pathname is a dir. Defaults to true.
+     */
+    redirect?: boolean;
+    /**
+     * Function to set custom headers on response. Alterations to the headers need to occur synchronously.
+     * The function is called as fn(res, path, stat), where the arguments are:
+     * res the response object
+     * path the file path that is being sent
+     * stat the stat object of the file that is being sent
+     */
+    setHeaders?: (res: express.Response, path: string, stat: any) => any;
+}
 export declare class ExpressServe {
     private name;
     private app;
     private options;
+    private _viewEngine;
     constructor(name: string, options?: IOptions);
-    accessLog(type?: AccessLogType, statusCode?: number): this;
-    router(mountPoint: string | RegExp, router: express.Handler): this;
+    accessLog(type?: AccessLogType, logMinStatusCode?: number): this;
+    router(mountPoint: string | RegExp | (string | RegExp)[], ...router: express.Handler[]): this;
+    serveStatic(mountPoint: string | RegExp | (string | RegExp)[], rootPath: string, options?: IServeStatic): this;
     errorHandle(handle?: IErrorHandle): this;
     private _preStart;
     onPreStart(action: IAction): this;
     private _postStart;
     onPostStart(action: IAction): this;
-    start(port: number): Promise<boolean>;
+    start(port?: number): Promise<boolean>;
+    viewengineSwig(rootDir: string, options?: IViewengineOptions): this;
+    crossdomain(options?: ICrossdomainOptions): this;
+}
+export interface IViewengineOptions {
+    cache?: boolean;
+    varControls?: [string, string];
+    tagControls?: [string, string];
+    cmtControls?: [string, string];
+}
+export interface ICrossdomainOptions {
+    allowDomain?: string[];
+    allowMethods?: string[];
+    allowHeaders?: string[];
 }
 export declare function api404(req: express.Request, res: express.Response, next: express.NextFunction): void;
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as morgan from 'morgan';
 import * as bluebird from 'bluebird';
 import * as errors from './error';
 
+const extend = require('extend');
 
 export {
     errors as IErrors
@@ -13,7 +14,7 @@ export enum AccessLogType {
 
 export interface IOptions {
     port?: number
-    errorStack?: boolean
+    //errorStack?: boolean
 }
 
 export interface IErrorHandle {
@@ -24,38 +25,104 @@ export interface IAction {
     (): Promise<void>
 }
 
+export interface IServeStatic {
+    /**
+     * Set how "dotfiles" are treated when encountered. A dotfile is a file or directory that begins with a dot (".").
+     * Note this check is done on the path itself without checking if the path actually exists on the disk.
+     * If root is specified, only the dotfiles above the root are checked (i.e. the root itself can be within a dotfile when when set to "deny").
+     * The default value is 'ignore'.
+     * 'allow' No special treatment for dotfiles
+     * 'deny' Send a 403 for any request for a dotfile
+     * 'ignore' Pretend like the dotfile does not exist and call next()
+     */
+    dotfiles?: string;
+
+    /**
+     * Enable or disable etag generation, defaults to true.
+     */
+    etag?: boolean;
+
+    /**
+     * Set file extension fallbacks. When set, if a file is not found, the given extensions will be added to the file name and search for.
+     * The first that exists will be served. Example: ['html', 'htm'].
+     * The default value is false.
+     */
+    extensions?: string[];
+
+    /**
+     * Let client errors fall-through as unhandled requests, otherwise forward a client error.
+     * The default value is false.
+     */
+    fallthrough?: boolean;
+
+    /**
+     * By default this module will send "index.html" files in response to a request on a directory.
+     * To disable this set false or to supply a new index pass a string or an array in preferred order.
+     */
+    index?: boolean | string | string[];
+
+    /**
+     * Enable or disable Last-Modified header, defaults to true. Uses the file system's last modified value.
+     */
+    lastModified?: boolean;
+
+    /**
+     * Provide a max-age in milliseconds for http caching, defaults to 0. This can also be a string accepted by the ms module.
+     */
+    maxAge?: number | string;
+
+    /**
+     * Redirect to trailing "/" when the pathname is a dir. Defaults to true.
+     */
+    redirect?: boolean;
+
+    /**
+     * Function to set custom headers on response. Alterations to the headers need to occur synchronously.
+     * The function is called as fn(res, path, stat), where the arguments are:
+     * res the response object
+     * path the file path that is being sent
+     * stat the stat object of the file that is being sent
+     */
+    setHeaders?: (res: express.Response, path: string, stat: any) => any;
+}
+
 export class ExpressServe {
     private app: express.Application = express();
     private options: IOptions = {
         port: 3000,
     };
+    private _viewEngine = false;
     constructor(private name: string, options?: IOptions) {
         if (options) {
             Object.assign(this.options, options)
         }
     }
 
-    accessLog(type?: AccessLogType, statusCode?: number) {
+    accessLog(type?: AccessLogType, logMinStatusCode?: number) {
         let morganType = type || AccessLogType.dev;
-        let skipStatusCode = statusCode || 400;
-
-        // console.log('AccessLogType', AccessLogType)
-        // console.log('AccessLogType[type]', AccessLogType[type], type)
 
         this.app.use(morgan(AccessLogType[type], {
             skip: (req, res) => {
                 if (req.query['enable_access_log']) {
                     return false;
                 }
-                return res.statusCode <= skipStatusCode;
+                if (logMinStatusCode === undefined || logMinStatusCode === null) {
+                    return false;
+                }
+                return res.statusCode <= logMinStatusCode;
             }
         }));
 
         return this;
     }
 
-    router(mountPoint: string | RegExp, router: express.Handler) {
+    router(mountPoint: string | RegExp | (string | RegExp)[], ...router: express.Handler[]) {
         this.app.use(mountPoint, router);
+        return this;
+    }
+
+    serveStatic(mountPoint: string | RegExp | (string | RegExp)[], rootPath: string, options?: IServeStatic) {
+        this.app.use(mountPoint, express.static(rootPath, options));
         return this;
     }
 
@@ -96,7 +163,8 @@ export class ExpressServe {
         return this;
     }
 
-    start(port: number): Promise<boolean> {
+    start(port?: number): Promise<boolean> {
+        let runPort = port || this.options.port;
         return new Promise<boolean>((done) => {
             bluebird.mapSeries(this._preStart, (action) => {
                 return action();
@@ -104,8 +172,8 @@ export class ExpressServe {
 
                 .then(() => {
                     return new Promise((resolve, reject) => {
-                        this.app.listen(port, () => {
-                            console.log(`starting name=${this.name}; port=${port}`);
+                        this.app.listen(runPort, () => {
+                            console.log(`starting name=${this.name}; port=${runPort}`);
                             resolve();
                         });
                     })
@@ -123,9 +191,67 @@ export class ExpressServe {
         })
 
     }
+
+    viewengineSwig(rootDir: string, options?: IViewengineOptions) {
+        if (this._viewEngine) {
+            throw new Error("view engine initlazed");
+        }
+        this._viewEngine = true;
+
+        let swig: { setDefaults: any, renderFile: any } = require('swig');
+        let defaultOptions: IViewengineOptions = {
+            cache: false,
+            varControls: ['[{', '}]'],
+            tagControls: ['[%', '%]'],
+            cmtControls: ['[#', '#]'],
+        };
+
+        let useOptions: IViewengineOptions = extend(defaultOptions, options || {});
+
+
+        this.app.engine('html', swig.renderFile);
+        this.app.set('view engine', 'html');
+        this.app.set('views', rootDir);
+        this.app.set('view cache', useOptions.cache);
+
+        swig.setDefaults({
+            varControls: useOptions.varControls,
+            tagControls: useOptions.tagControls,
+            cmtControls: useOptions.cmtControls,
+            cache: useOptions.cache
+        });
+        return this;
+    }
+    crossdomain(options?: ICrossdomainOptions) {
+        let domains = (options && options.allowDomain) || ['*'];
+        let methods = (options && options.allowMethods) || ['GET', 'PUT', 'POST', 'DELETE'];
+        let headers = (options && options.allowHeaders) || ['Content-Type', 'Authorization'];
+
+        this.app.use((req: express.Request, res: express.Response, next) => {
+            res.header('Access-Control-Allow-Origin', domains.join(' '));
+            res.header('Access-Control-Allow-Methods', methods.join(','));
+            res.header('Access-Control-Allow-Headers', headers.join(','));
+            if ('OPTIONS' === req.method) {
+                res.status(200).end();
+            } else {
+                next();
+            }
+        })
+        return  this;
+    }
 }
 
-
+export interface IViewengineOptions {
+    cache?: boolean
+    varControls?: [string, string]
+    tagControls?: [string, string]
+    cmtControls?: [string, string]
+}
+export interface ICrossdomainOptions {
+    allowDomain?: string[]
+    allowMethods?: string[]
+    allowHeaders?: string[]
+}
 
 export function api404(req: express.Request, res: express.Response, next: express.NextFunction) {
     next(new errors.NotFound(`not found uri: "${req.originalUrl}"`));
